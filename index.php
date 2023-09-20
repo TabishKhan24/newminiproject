db8b7cc1d108b78707907a6e30e50252e07350bd
TabishKhan24 <khantabish2448@gmail.com>
2023-09-20 10:39:41 +0530

    edited

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a href="https://www.Diems">Diems=sdsddsklklkl</a>.</strong>
+    <strong>Copyright &copy; 2023 <a href="https://www.Diems">Diems</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b><?php echo $_SESSION['system']['name'] ?></b>
